6a23fc4c1bdb928497b14e8c6c8ed4f0208605f4
Anh Tran <anhtr077@gmail.com>
2023-02-27 13:56:22 -0800

    use argparse to handle arguments

---
+++ rpchecker/cli.py
@@ -0,0 +1,43 @@
+# cli.py
+
+import argparse
+
+def read_user_cli_args():
+    """Handle the CLI arguments and options."""
+    parser = argparse.ArgumentParser(
+        prog="rpchecker",
+        description="check the availability of websites"
+    )
+    parser.add_argument(
+        "-u",
+        "--urls",
+        metavar="URLs",
+        nargs="+",
+        type=str,
+        default=[],
+        help="enter one or more website URLs",
+    )
+    parser.add_argument(
+        "-f",
+        "--input-file",
+        metavar="FILE",
+        type=str,
+        default="",
+        help="read URLs from a file"
+    )
+    parser.add_argument(
+        "-a",
+        "--asynchronous",
+        action="store_true",
+        help="run the connectivity check asynchronously",
+    )
+
+    return parser.parse_args()
+
+def display_check_result(result, url, error=""):
+    """Display the result of a connectivity check."""
+    print(f'The status of "{url}" is:', end=" ")
+    if result:
+        print('"Online!" 🙂')
+    else:
+        print(f'"Offline?" 🫥 \n Error: "{error}"')
